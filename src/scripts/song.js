a326a6cfe424f330998a8b848005b70f72e9aab9
mikeyQwn <mikeyQwn@gmail.com>
2022-12-02 18:32:09 +0300

    Play button now plays an oscillator generated tune

--- src/scripts/song.js
+++ src/scripts/song.js
@@ -1,3 +1,6 @@
+import { playNote } from "./synthesizer.js";
+import { getNoteFrequency } from "./utils/noteString.js";
+
 export class Song {
     constructor(name, tempo, timeSignature, tabulature) {
         this.name = name;
@@ -10,13 +13,20 @@ export class Song {
         const genericNotes = [];
         this.tabulature
             .getArrayOfGenericNotes()
-            .forEach((el) => console.log(el));
+            .forEach((note) => genericNotes.push(note));
+        return genericNotes;
     }
 
     playSong() {
         console.log("Hey!");
         console.log(this.tabulature);
         const calculatedSong = this.calculateSong();
+        calculatedSong.forEach((genericNote, i) =>
+            setTimeout(
+                () => playNote(getNoteFrequency(genericNote.note), "sine"),
+                250 * i
+            )
+        );
     }
 }
 
--- src/scripts/synthesizer.js
+++ src/scripts/synthesizer.js
@@ -2,12 +2,13 @@ const context = new AudioContext();
 let o;
 let g;
 
-export function playNote(frequency, type, duration = 1) {
+export function playNote(frequency, type, duration = 2) {
+    console.log(frequency);
     o = context.createOscillator();
     g = context.createGain();
     o.type = type;
     o.connect(g);
-    g.gain.value = 1;
+    g.gain.value = 0.5;
     o.frequency.value = frequency;
     g.connect(context.destination);
     o.start(0);
