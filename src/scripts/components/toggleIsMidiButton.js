3f87e790e2f711d3a87c023fe00b1b473810d1ea
mikeyQwn <mikeyQwn@gmail.com>
2022-12-11 20:53:47 +0300

    Added midi button text

--- src/scripts/components/toggleIsMidiButton.js
+++ src/scripts/components/toggleIsMidiButton.js
@@ -1,5 +1,6 @@
 export const toggleIsMidiButton = {
     song: null,
+    isMidi: true,
     setSong: (song) => {
         toggleIsMidiButton.song = song;
     },
@@ -7,8 +8,13 @@ export const toggleIsMidiButton = {
         const song = toggleIsMidiButton.song;
         const toggleIsMidiButtonEl = document.createElement("button");
         toggleIsMidiButtonEl.classList.add("toggle-midi-button");
+        toggleIsMidiButtonEl.innerText = "Midi:on";
         toggleIsMidiButtonEl.onclick = () => {
             song.audioManager.toggleIsMidi();
+            toggleIsMidiButton.isMidi = !toggleIsMidiButton.isMidi;
+            toggleIsMidiButtonEl.innerText = `Midi:${
+                toggleIsMidiButton.isMidi ? "on" : "off"
+            }`;
         };
         document.body.appendChild(toggleIsMidiButtonEl);
     }
