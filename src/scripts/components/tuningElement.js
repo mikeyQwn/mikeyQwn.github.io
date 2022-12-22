cfd4243066e4860e4e96bb903b9a5c69226172d2
mikeyQwn <mikeyQwn@gmail.com>
2022-12-22 14:34:17 +0300

    fix: fixed tunining element render

--- src/scripts/components/tuningElement.js
+++ src/scripts/components/tuningElement.js
@@ -2,16 +2,18 @@ const firstRowContainer = document.getElementById("first-tab-row-container");
 
 export const tuninigElement = {
     song: null,
+    tuninigElementContaier: null,
     setSong: (song) => {
         tuninigElement.song = song;
     },
     render: () => {
+        if (tuninigElement.tuninigElementContaier) return;
         const tuning = tuninigElement.song.getTabulatureObject().getTuning();
         const tuningEl = document.createElement("div");
         tuningEl.classList.add("tuning-element");
         tuningEl.classList.add("unselectable");
         firstRowContainer.appendChild(tuningEl);
-        console.log(tuningEl);
         tuningEl.innerText = tuning;
+        tuninigElement.tuninigElementContaier = tuningEl;
     }
 };
